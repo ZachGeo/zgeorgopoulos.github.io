c7a4d8da374539da7646423041bfae61877f3bb1
ZachGeo <zachageo95@gmail.com>
2021-01-29 18:57:13 +0200

    Initial commit

---
+++ static/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    $('i[data-toggle="tooltip"]').tooltip({
+        animated: 'fade',
+        trigger: 'click'
+    });
+});
+
+$(function () {
+    $(document).on('shown.bs.tooltip', function (e) {
+       setTimeout(function () {
+         $(e.target).tooltip('hide');
+       }, 3000);
+    });
+ });
+
+$(document).ready(function(){
+  $(".flex_head").hide();
+  $(".flex_head").fadeIn(2000);
+});
+
+$(document).ready(function(){
+  $(".about_p").hide();
+  $(".about_p").fadeIn(1000);
+});
+
+$(document).ready(function(){
+  $(".fa-python").hide();
+  $(".fa-python").fadeIn(1000);
+});
+
+$(document).ready(function(){
+  $(".fa-hashtag").hide();
+  $(".fa-hashtag").fadeIn(1500);
+});
+
+$(document).ready(function(){
+  $(".fa-database").hide();
+  $(".fa-database").fadeIn(2000);
+});
+
+$(document).ready(function(){
+  $(".fa-git").hide();
+  $(".fa-git").fadeIn(2500);
+});
+
+$(document).ready(function(){
+  $(".fa-window-maximize").hide();
+  $(".fa-window-maximize").fadeIn(3000);
+});
+
+$(document).ready(function(){
+  $(".fa-linux").hide();
+  $(".fa-linux").fadeIn(3500);
+});
